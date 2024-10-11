6ee46930f0f1fb5ed5cae626970b0c31fcbcf037
Ednut <147618496+Ednut001@users.noreply.github.com>
2024-10-11 11:15:54 +0100

    Add files via upload

--- all/place.js
+++ all/place.js
@@ -2,6 +2,7 @@ require("./global")
 
 const store = makeInMemoryStore({ logger: pino().child({ level: 'silent', stream: 'store' }) })
 const delay = ms => (ms) && new Promise(resolve => setTimeout(resolve, ms))
+const makeid = crypto.randomBytes(3).toString('hex')
 
 const getBuffer = async (url, options) => {
 try {
